ce9b4428398510a6988143361a9d9b84cfb634cd
jaba <jabaadamia1@gmail.com>
2025-03-19 15:27:57 +0400

    added leaderboard

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
 
+from users.models import CustomUser
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["top10"] = CustomUser.objects.order_by("-puzzle_rating")[:10]
+        return context
